86be878604e2a88153cbabe23db10deb28a8a314
Teisia Park <teisia@gmail.com>
2015-12-21 16:12:54 -0700

    added user info fields

--- public/js/orders.js
+++ public/js/orders.js
@@ -6,9 +6,19 @@ var menu = $.ajax({
 
 menu.done(function(response) {
   var items = response["menu"];
+  var itemsArray = [];
     for (var i = 0; i < items.length; i++) {
+//      itemsArray.push(items[i]["type"]);
+  //var unique = itemsArray.filter(function(elem, pos) {
+    //return itemsArray.indexOf(elem) == pos;
+//})
       $(".menuItems").append("<option disabled>"+items[i]['type']+"</option>");
       $(".menuItems").append("<option value>"+items[i]['name']+" "+items[i]['price']+"</option>");
       $('select option:nth-child(2)').attr("selected", "selected");
+
 }
 })
+
+$("#addMe").click(function() {
+  $(".selectedItems").append("");
+  })
